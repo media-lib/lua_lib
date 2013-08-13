5b8a63e2ef3bbb9711ce4c1ffeb9dcb497a6c5b6
da99 <i-hate-spam-1234567@mailinator.com>
2013-08-12 22:18:11 -0500

    Touchups & more doc.

--- templates/index.rb
+++ templates/index.rb
@@ -7,6 +7,31 @@ main {
   h1 'Sinatra.lua'
   h2 'Sinatra takes a little break from the Ruby stage to sing a diddy under the Brazilian moon.'
 
+  p {
+    span.note "Note:"
+    span "
+      This site is unofficial and has no association
+      with
+    "
+    a("Sinatra.rb", :href=>"http://www.sinatrarb.com")
+    span " or "
+    a("Sinatra-Openresty", :href=>"https://github.com/jtarchie/sinatra-openresty")
+  }
+
+  div.install {
+    h3 "Install Lua:"
+    p "On Ubuntu:"
+    highlight "sh", "sudo apt-get install lua5.1"
+
+    h3 "Install OpenResty:"
+    p {
+      span "Go to: "
+      a("http://openresty.org/#Installation", :href=>"http://openresty.org/#Installation")
+    }
+
+  }
+
+  h3 "Hello, crooner."
   highlight('lua',  <<-eos
 local sinatra = require('sinatra');
 

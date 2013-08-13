135013fdf36fcceac1b0c93533ad17cc4ef55c1f
da99 <i-hate-spam-1234567@mailinator.com>
2013-08-12 23:08:15 -0500

    Send your complaints to the memory hole.

--- templates/index.rb
+++ templates/index.rb
@@ -29,6 +29,13 @@ main {
       a("http://openresty.org/#Installation", :href=>"http://openresty.org/#Installation")
     }
 
+    h3 "Install sinatra-openresty:"
+    p {
+      span "Your guess is as good as mine. "
+      span "("
+      span.note "Seriously: "
+      span "I'll post more about this later.)"
+    }
   }
 
   h3 "Hello, crooner."
@@ -45,11 +52,23 @@ app:get("/:name", function()
   return "Hello, " .. params.name;
 end)
 
-app:get("/age/:age", function(age)
-  return "You are " .. tostring(age) .. " years old."
+app:get("/drinks/:num", function(num)
+  return "I ordered " .. tostring(num) .. " Mint Juleps."
 end)
 
 app:run()
 eos
 )
+   h3 "Alternatives:"
+   p {
+     a("Lapis", :href=>"http://leafo.net/lapis/")
+     span ": Another easy to use framework on OpenResty."
+   }
+
+   h3 "More to come:"
+   p {
+     span "That's all for now. "
+     span "Send complaints, suggestions, and hate mail to: "
+     a("sin-moon", :href=>"https://github.com/sin-moon/sin-moon.github.io/issues")
+   }
 }

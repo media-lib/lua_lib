e49c607433afa77bbd195da0897c93c43abd01dc
da99 <i-hate-spam-1234567@mailinator.com>
2016-03-07 02:58:07 -0600

    Started: getting ready to merge with my lua_lib repo

--- Gemfile
+++
@@ -1,6 +0,0 @@
-
-source 'https://rubygems.org'
-gem 'dot_why'
-gem 'pygments.rb'
-gem 'thin'
-gem 'sinatra'
--- dev_server/sin.rb
+++
@@ -1,7 +0,0 @@
-require 'sinatra'
-
-use Rack::Static, :urls => ["/favicon.ico", "/css", "/img"], :root => "./"
-
-get '/' do
-  File.read("index.html")
-end
